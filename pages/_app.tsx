0fedc5b236ed8afd6b68d421497fc291b9c1aaf8
Yama022 <r=jacquemin@hotmail.fr>
2024-07-08 11:16:08 +0200

    update title app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>TCG Market Value</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
